929c57b7da04a4d017c539d5ffa648523cae9e9c
Kamil Krupski <krupski102@gmail.com>
2016-05-25 13:29:50 +0200

    new tests and solve problems

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -7,30 +7,38 @@ describe('app', function ()
     describe('upperCase', function () {
         it('should converts the first letter each word in upper case', function () {
             expect(exercise.upperCase('This is a sentence')).toBe('This Is A Sentence');
+            expect(exercise.upperCase('kamil krupski')).toBe('Kamil Krupski');
         });
         it('should not change upper case letter', function () {
             expect(exercise.upperCase('ThisIs Hard')).toBe('ThisIs Hard');
+            expect(exercise.upperCase('ThisIs easy')).toBe('ThisIs Easy');
         });
         it('should not change punctuation', function () {
-            expect(exercise.upperCase('This is,m Hard')).toBe('This Ism Hard');
+            expect(exercise.upperCase('This is,m Hard')).toBe('This Is,m Hard');
+            expect(exercise.upperCase('This m. easy')).toBe('This M. Easy');
         });
     });
 
     describe('findAlmostMax', function () {
         it('should return second greatest number', function () {
             expect(exercise.findAlmostMax([2, 3, 4])).toBe(3);
+            expect(exercise.findAlmostMax([5, 6, 7])).toBe(6);
         });
         it('should return second greatest number', function () {
-            expect(exercise.findAlmostMax([50, 40, 30, 10])).toBe(50);
+            expect(exercise.findAlmostMax([50, 40, 30, 10])).toBe(40);
+            expect(exercise.findAlmostMax([20, 70, 50, 10])).toBe(50);
         });
         it('string should be omnitted', function () {
-            expect(exercise.findAlmostMax([50, 'str', 30, 10])).toBe('str');
+            expect(exercise.findAlmostMax([50, 'str', 30, 10])).toBe(30);
+            expect(exercise.findAlmostMax([50, 'str', 70, "str2"])).toBe(50);
         });
         it('empty array should return false', function () {
-            expect(exercise.findAlmostMax([])).toEqual(true);
+            expect(exercise.findAlmostMax([])).toEqual(false);
+            expect(exercise.findAlmostMax([ , ])).toEqual(false);
         });
         it('array of string should return false', function () {
             expect(exercise.findAlmostMax(['cat', 'kot'])).toEqual(false);
+            expect(exercise.findAlmostMax(['cat', 'kot', 'pies'])).toEqual(false);
         });
     });
 });
